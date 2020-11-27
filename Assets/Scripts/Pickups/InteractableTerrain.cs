e5fd9b3fd356040b6d16d9aa11e7f0a29765c3bf
Levi Stenton <levistenton@gmail.com>
2020-11-28 01:24:20 +1000

    This stuff was meant to be in the last commit

---
+++ Assets/Scripts/Pickups/InteractableTerrain.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableTerrain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public BoxCollider cc;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        if(GetComponent<BoxCollider>() == null)
+            gameObject.AddComponent<BoxCollider>();
+        cc = GetComponent<BoxCollider>();
+        cc.isTrigger = true;
+    }
+
+    public virtual void OnTriggerEnter(Collider other) 
+    {
+
+    }
+
+    public virtual void OnTriggerStay(Collider other) 
+    {
+        
+    }
+
+    public virtual void OnTriggerExit(Collider other) 
+    {
+        
+    }
+}
